387fa9a9294e6fe285918787c1e97b0d41ce58cf
ZHTGitHub <chat195@163.com>
2019-10-01 09:16:02 +0800

    实现随机粒子

--- canvas.js
+++ canvas.js
@@ -1,5 +1,47 @@
 ;!(function() {
 
-    console.log('使用 Canvas 制作出炫酷的网页背景特效')
+    var canvas = document.getElementById('canvas')
+    var context = canvas.getContext('2d')
+    var canvasW = document.documentElement.clientWidth
+    var canvasH = document.documentElement.clientHeight
+    var amount = 100
+    var star = []
+
+    canvas.width = canvasW
+    canvas.height = canvasH
+
+    function Stars(x, y) {
+
+        this.x = x
+        this.y = y
+        this.r = Math.random() * 2 + 1
+        var alpha = Math.floor(Math.random()*10+1) / 10 / 2
+        this.color = 'rgba(255, 255, 255, '+ alpha +')'
+
+    }
+
+    Stars.prototype.draw = function() {
+
+        context.fillStyle = this.color
+        context.shadowBlur = this.r * 2
+        context.beginPath()
+        context.arc(this.x, this.y, this.r, 0, 2*Math.PI, false)
+        context.closePath()
+        context.fill()
+
+    }
+
+    function init() {
+
+        for(let i = 0; i < amount; i++) {
+
+            star[i] = new Stars(Math.random()*canvasW, Math.random()*canvasH)
+            star[i].draw()
+
+        }
+
+    }
+
+    init()
 
 })();
